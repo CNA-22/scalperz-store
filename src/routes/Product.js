a7dbd4309ff30c3165284736f90e4557b5d94eec
Fredrik Wasström <fredrik.wasstrom@gmail.com>
2022-03-08 13:45:12 +0200

    W.I.P. Add conditional rendering for out of stock

--- src/routes/Product.js
+++ src/routes/Product.js
@@ -4,7 +4,7 @@ import { generateStarRating } from '../Utils/Common';
 import CartButton from './Components/CartButton';
 
 const Product = ({name, price, desc, rating, imageUrl, id}) => {
-
+    const inStock = ({inStock: 3}.inStock > 0) //hardcoded data until inventory service works again, this will be replaced by a request and a state
     const description = desc.substr(0, 200)
     return(
     <div className="product">
@@ -19,7 +19,7 @@ const Product = ({name, price, desc, rating, imageUrl, id}) => {
             <p>{price}€</p>
             <p>{description}...<Link to={"/product/"+id}>read more</Link> </p>
             <p>{generateStarRating(rating)}</p>
-            <CartButton id={id}/>
+            <CartButton id={id} active={inStock}/>
         </div>
 
     </div>
