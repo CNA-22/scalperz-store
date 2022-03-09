b8e9f1614fe1f536cfcdac0efbbec9d55d18ef57
SikiPriki <halliksi@arcada.fi>
2022-03-09 18:42:42 +0200

    New try with all products

--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -4,12 +4,12 @@ import React from "react"
 import './styles/Checkout.css'
 import { useState } from 'react';
 import { useCookies } from "react-cookie";
+import { Next } from 'react-bootstrap/esm/PageItem';
 
 
 const axios = require('axios');
 
 const Checkout = () => {
-
 
   //Får JWT
   const getCookieToken = () => {
@@ -22,190 +22,156 @@ const Checkout = () => {
   const [inputEmail, setInputEmail] = useState('');
   const [inputAd1, setInputAd1] = useState('');
   const [userid, setUserid] = useState('');
-  const [useremail, setUseremail] = useState();
+  const [useremail, setUseremail] = useState('');
 
   //Cart saker
   const [cartProductId, setProductId]=useState([]);
- 
+  const [cartProductName, setCartProductName]=useState([]);
   const [cartProductAmount, setCartProductAmount]=useState([]);
 
   //Produkt saker
-  const [productPrice, setPrice] = useState([]);
-  const [cartProductName, setCartProductName]=useState([]);
+  const [productPrice, setPrice] = useState();
+ 
 
-  const [totalPrice, setTotal] = useState();
-
-  const [proData, setproData]=useState([]);
-  
-/*
-  axios.get("https://quiet-meadow-01451.herokuapp.com/orders",{
-  headers: {
-  'Authorization': `Bearer ${access_token}` 
-  }}).then((res)=>{console.log(res.data)})
-*/
   //Få användar info
   //https://flaviocopes.com/axios-send-authorization-header/
   const callUser=()=>{
-    
       axios.get('https://cna22-user-service.herokuapp.com/users/data', {
       headers: {
         'Authorization': `Bearer ${access_token}`
       }
-      }).then((res) => {
+      })
+      .then((res) => {
        
         setUserid(tokenToJson(access_token).sub)
         setUseremail(tokenToJson(access_token).email)
-       //if(inputAd1.length===0){ findAdress(useremail,res.data)}
-       findAdress(useremail,res.data)
+        findAdress(useremail,res.data)
 
       })
       .catch((error) => {
       console.error(error)
       })
-    
   }
-
-
-  
-//Hantera token
-const tokenToJson= (token) =>{
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(atob(base64));
-}
-
-//Hitta användar address
-const findAdress=(mail,data)=>{
-  
-    for(var i=0;i<data.length;i++){
-      if(mail===data[i].email){
-        console.log(data[i].email)
-        setInputEmail(data[i].email)
-      setInputAd1(data[i].adress+' '+data[i].zip)
-      }
-    }
-  
-}
+  callUser()
 console.log(userid)
-callUser()
-
-
-  /*  axios.get("https://quiet-meadow-01451.herokuapp.com/orders",{
-      headers: {
-        'Authorization': `Bearer ${access_token}`
-      }})
-      .then((res) => {
-        console.log(res.data)
-      })*/
-
-
-const proId=[]
-const amountD=[]
-const pri=[]
-const na=[]
+//TODO: Få flera saker.
 
   const cartInfoo=()=>{
-
-   if(cartProductAmount.length===0){
+    if(cartProductAmount.length<1){
     axios.get(`https://cna-cart-api.herokuapp.com/cart/${userid}`,{//använd 2 om din id ej hittas i cart att se att koden funkar${userid}
       headers: {
         'Authorization': `Bearer ${access_token}` 
       }
       })
       .then((result) => { 
-        //cartInforma(result.data)
-        const data=result.data
-        for(let i=0; i<data.length;i++){
-          proId.push(data[i].pId)
-          //console.log(proId)
-          amountD.push(data[i].productAmount)
-          
+        let proId=[]
+        let prodA=[]
+
+        for(var i=0;i<result.data.length;i++)
+        {
+          prodA.push(result.data[i].productAmount)
+          proId.push(result.data[i].pId)
         }
-        console.log(proId)
-        //setProductId(proId)
-    
-      //  if (cartProductName.length===0){getPrice(proId,amountD)}
-      getPrice(proId,amountD)
+        
+        setCartProductAmount(prodA)
         setProductId(proId)
-        setCartProductAmount(amountD)
+        
+        //setProductId(result.data[0].pId)
+       // console.log(result.data[0].pId)
+       
+        //setCartProductAmount(result.data[0].productAmount)
+        //console.log(result.data[0].productAmount)
+
       })
       .catch((error) => {
-        console.error(error)
-      })
-   }
-  } 
-
-  //if(cartProductAmount.length==0){cartInfoo()}
-  cartInfoo()
-
-/*  const cartInforma=(data)=>{
-  
-    for(let i=0; i<data.length;i++){
-      proId.push(data[i].pId)
-      //console.log(proId)
-      amountD.push(data[i].productAmount)
-      
-    }
-    console.log(proId)
-    //setProductId(proId)
-
-    getPrice(proId,amountD)
-    setProductId(proId)
-    setCartProductAmount(amountD)
-  //setCartProductName( getPrice(proId,amountD))
-}
-*/
-  //Få länk till producter
-  const getPrice= (proId,proAm)=>{
-    if (cartProductName.length===0){
-      axios.get(`https://cna22-products-service.herokuapp.com/products`,{//${proId[j]},{// Ändra till cookies.item_id om något ej funkar att se att man faktist får info t.e.x när cart ej har saken i sig
-      headers: {
-        'Authorization': `Bearer ${access_token}` 
-      }
-      }).then((res)=>{
-        getNamesAndPrices(res.data.items,proId,proAm)
-        
-        
+      console.error(error)
       })
     }
   }
- // setCartProductName(cartProductName=>[ cartProductName, na])
 
 
- const getNamesAndPrices=(data,produId,amount)=>{
+  if(cartProductAmount.length<1){cartInfoo()}
+
+console.log(cartProductAmount)
+
+//Hantera token
+  const tokenToJson= (token) =>{
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+  }
   
-    let total=0
-    for(var i=0; i<data.length;i++){
-      if(data[i].pid===produId[i]){
-        na.push(data[i].name)
-        pri.push(Number(data[i].price*amount[i]))
-        total=Number(total)+(Number(data[i].price)*Number(amount[i]))
+//Hitta användar address
+  const findAdress=(mail,data)=>{
+      for(var i=0;i<data.length;i++){
+        if(mail===data[i].email){
+          //console.log(data[i])
+          setInputEmail(data[i].email)
+        setInputAd1(data[i].adress+' '+data[i].zip)
+        }
       }
+  }
+
+
+//Cookie för om man behöver testa något eller sakerna vid test skedje ej finns
+
+
+  //Räkna ut priset
+  const getPrice= ()=>{
+    if(cartProductName.length<1){
+      let newPris=0
+      const newName=[]
+      //const newPris = []
+      let proAmo=cartProductAmount
+     
+      
+      for(var j=0; j<cartProductId.length;j++){
+      
+        axios.get(`https://cna22-products-service.herokuapp.com/product/${cartProductId[j]}`,{
+        headers: {
+          'Authorization': `Bearer ${access_token}` 
+        }
+        })
+        .then((res)=>{
+          
+           console.log([j])
+           console.log(Object.values(proAmo))
+            newPris=(newPris)+(res.data.price*((cartProductAmount[0]))) //Vill ej funka med [j] för mig iallafall
+            console.log(typeof (cartProductAmount[0]));
+            console.log(typeof newPris);
+            console.log(typeof res.data.price);
+            console.log(newPris)
+            newName.push(res.data.name)
+           setCartProductName(oldArray => [...oldArray, res.data.name]);
+           setPrice(newPris)  
+               //return newName   
+        })
+      
+      }  
+
+      //setCartProductName(newName);
+      console.log(newPris)
+      
     }
-    console.log(pri)
-    setCartProductName(na)
-    console.log(cartProductName.length)
-    setPrice(pri)
-    setTotal(total)
+    
+  }
   
- }
+  if(cartProductName.length<1){ getPrice()}
+  console.log(cartProductName)
+  console.log(productPrice)
 
-
- 
   //Hanterar submit
-  const onSubmit=(event)=>{
+  const handleSubmit=(event)=>{
     event.preventDefault();
-
-    for(var j=0;j<cartProductId.length; j++)
-      axios.post("https://quiet-meadow-01451.herokuapp.com/orders",{
-        
-                    customerNumber: userid,
-                    email:inputEmail,
-                    itemId: cartProductId[j],
-                    address: inputAd1,
-                    price: productPrice[j]
-      },{headers: {
-        'Authorization': `Bearer ${access_token}` 
+    axios.post("https://quiet-meadow-01451.herokuapp.com/orders",{
+      
+                  customerNumber: userid,
+                  email:inputEmail,
+                  itemId: cartProductId[0],
+                  address: inputAd1,
+                  price: productPrice
+    },{headers: {
+      'Authorization': `Bearer ${access_token}` 
     }})
    
     //Confirmation info
@@ -221,16 +187,17 @@ const na=[]
          inputForm.style.display = 'block';
       }
   }
+ 
 
   return (
       <div >
           <div>
+
               <div id={'purchaseDiv'}>
-                <p>Delivery information</p>
-                <p>If this is the first time being on this site, please reload to get the cookie information</p>
-                <b>Product(s):</b> {cartProductName+'\n'}
-                <br></br>
-                <form name={'confirmationForm'} onSubmit={onSubmit}>
+     
+                
+                <p><b>Product(s):</b> {cartProductName}</p>
+                <form name={'confirmationForm'} onSubmit={handleSubmit}>
                     <h3>Email</h3>   
                     <input value={inputEmail} readOnly  />
                     <h3>Address</h3>
@@ -239,7 +206,7 @@ const na=[]
                     <br></br>
                     <input className='confirmationButton' type={'submit'} value={'Submit'}></input>
                  </form>
-                <p>Price: {totalPrice} EUR</p>
+                <p><b>Price:</b> {productPrice} EUR</p>
                 <br></br>
              
               </div>
@@ -247,15 +214,14 @@ const na=[]
 
               <div id={'confirmationDiv'} >
                 <p><b>We has sent the invoice to:</b> {inputEmail} </p>
-                <p><b>Delivering to:</b>  {inputAd1}</p>
-                <p><b>Products:</b>  {cartProductName+"\n"} </p>
-                <p><b>Price:</b>  {totalPrice}</p>
+                <p><b>Delivering to:</b> {inputAd1}</p>
+                <p><b>Products:</b> {cartProductName} </p>
+                <p><b>Price:</b> {productPrice} EUR</p>
               </div>  
           </div>
       </div> 
   );
- 
-
 }
+
 
 export default Checkout
